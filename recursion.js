4f4121fd1ee869f34d4c9c954fbb3e552660a902
lfrederick034 <lfrederick220@gmail.com>
2025-08-11 18:55:40 -0500

    Fixing a mistake

--- recursion.js
+++ recursion.js
@@ -4,12 +4,10 @@
 function reverseString(word) {
     // Base Case
     if(word.length === 1 || word === "") {
-        console.log("Length: ");
-        console.log(word);
         return word;
     }
 
-    return (word[word.length - 1] + reverseString(word.slice(0, -1), count));
+    return (word[word.length - 1] + reverseString(word.slice(0, -1)));
      
 }
 
