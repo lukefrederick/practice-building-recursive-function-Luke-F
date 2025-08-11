c60b7c51c646266149701fd2fc59684a1147bd92
lfrederick034 <lfrederick220@gmail.com>
2025-08-11 15:51:41 -0500

    Forgot to clean up the code

--- recursion.js
+++ recursion.js
@@ -1,48 +1,18 @@
 
 
 
-function reverseString(word, count) {
-    console.log("Count: " + count);
-    count++;
-    console.log("Word: " + word)
-    if (count > 10) {
-        return;
-    }
+function reverseString(word) {
     // Base Case
     if(word.length === 1 || word === "") {
         console.log("Length: ");
         console.log(word);
         return word;
     }
-    // Concatenate the last character with the result of the recursive call on the remaining part of the string (recursive case)
 
     return (word[word.length - 1] + reverseString(word.slice(0, -1), count));
      
 }
 
-let res = reverseString("hello", 0);
+let res = reverseString("hello");
 console.log(res);
 
-/*
-hello
-
-result = o, pass in hell
-call
-reslt = ol, pass in hel
-call
-result = oll, pass in he
-result = olle pass in h
-result = olleh pass in ''
-return
-
-
-
-
-
-
-
-
-
-
-
-*/
